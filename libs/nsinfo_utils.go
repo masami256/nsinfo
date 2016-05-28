19923d2df21bf90ad1c04b9c0b5d85c72139d035
Masami Ichikawa <masami256@gmail.com>
2016-05-28 23:16:01 +0900

    Add directory walker feature.

--- libs/nsinfo_utils.go
+++ libs/nsinfo_utils.go
@@ -2,7 +2,74 @@ package nsinfo
 
 import (
     "os"
+    "io/ioutil"
+    "path/filepath"
+    "regexp"
+    "fmt"
+    "strings"
 )
+
+const proc_dir = "/proc"
+
+type NamespaceInfo struct {
+    ns map[string]string
+}
+
+type ProcessMap struct {
+    process map[string]NamespaceInfo
+}
+
+func lookupNamespaceDirectory(processes *ProcessMap, pid string) {
+    nsdir := filepath.Join(proc_dir, pid, "ns")
+    namespaces, err := ioutil.ReadDir(nsdir)
+
+    if err != nil {
+        panic("cannot read ns directory")
+    }
+
+    nsinfo := NamespaceInfo{}
+    nsinfo.ns = map[string]string{}
+
+    for _, ns := range namespaces {
+        path := filepath.Join(nsdir, ns.Name())
+        value, err := os.Readlink(path)
+
+        if err != nil {
+            panic("faild to readlink")
+        }
+        nsinfo.ns[ns.Name()] = strings.Split(strings.Split(value, "[")[1], "]")[0]
+    }
+    processes.process[pid] = nsinfo
+}
+
+func walkProcDirectory() {
+    processes := ProcessMap{}
+    processes.process = map[string]NamespaceInfo{}
+
+    files, err := ioutil.ReadDir(proc_dir)
+    if err != nil {
+        panic("Failed to get directories")
+    }
+
+    for _, f := range files {
+        if regexp.MustCompile("^[1-9]").Match([]byte(f.Name())) {
+            lookupNamespaceDirectory(&processes, f.Name())
+        }
+    }
+
+    for pid, nsinfo := range processes.process {
+        fmt.Printf("pid: %s\n", pid)
+        for ns, nsval := range nsinfo.ns {
+            fmt.Printf("%s: %s, ", ns, nsval)
+        }
+        println("")
+    }
+
+}
+
+func ShowAllNamespaces() {
+    walkProcDirectory()
+}
 
 func IsRoot() bool {
     return os.Getuid() == 0
